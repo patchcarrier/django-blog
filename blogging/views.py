a02bd1dff47bd5d450765a0823c56d714029b0a3
Patrick Carrier <patrick.carrier16@gmail.com>
2022-10-09 13:47:22 -0700

    Changing blogging views from function-based to class-based

--- blogging/views.py
+++ blogging/views.py
@@ -1,32 +1,18 @@
-from django.template import loader
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from blogging.models import Post
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
 
-
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
-
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    # DetailView.get_object() will only try and find a Post matching pk if it's
+    # in the set of Posts that have been published
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
+   
+    
+class BlogListView(ListView):
+    # Only show posts that have been published, and display them in reverse
+    # chronological order
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
